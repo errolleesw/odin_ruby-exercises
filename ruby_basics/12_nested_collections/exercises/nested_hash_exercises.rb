ef0aa767ea5a11971b165b6bea9201ee44d4a13a
Errol Lee <62831245+errolleesw@users.noreply.github.com>
2023-03-26 10:36:52 +1100

    completed 12 nexted collections

--- ruby_basics/12_nested_collections/exercises/nested_hash_exercises.rb
+++ ruby_basics/12_nested_collections/exercises/nested_hash_exercises.rb
@@ -7,32 +7,53 @@ def find_language_information(languages, language_name)
   #   ruby: { initial_release: 'December 25, 1996', is_beautiful?: true },
   #   javascript: { initial_release: 'December 4, 1995', is_beautiful?: false }
   # }
+  languages[language_name]
 end
 
 def add_information_about_language(languages, language_name, info_key, info_value)
   # Take languages and add the key/value pair info_key/info_value to the nested
   # hash of language_name, then return the updated languages hash
+  languages[language_name][info_key] = info_value
+  languages
 end
 
 def add_language(languages, language_name, language_info_value)
   # Take languages and add the key/value pair language_name/language_info_value
   # to it, then return languages
+  languages[language_name] = language_info_value
+  languages
 end
 
 def delete_information_about_language(languages, language_name, info_key)
   # Take languages and delete the key/value pair with key info_key from
   # language_name, then return languages
+  languages[language_name].delete(info_key)
+  languages
 end
 
 def delete_language(languages, language_name)
   # Take languages and delete the language_name key/value pair, then return
   # languages
+  languages.delete(language_name)
+  languages
 end
 
 def find_beautiful_languages(languages)
   # Take languages and return a hash containing only languages which have the
   # key/value pair { is_beautiful?: true } listed in their information
+  languages.select { |language, data| data[:is_beautiful?] == true}
 end
+
+# hash = {
+#   ruby: { is_beautiful?: true, initial_release: 'December 25, 1996' },
+#   javascript: { is_beautiful?: false, initial_release: 'December 4, 1995' },
+#   python: { is_beautiful?: false, initial_release: 'Feb 20, 1991' }
+# }
+
+# p find_beautiful_languages(hash)
+
+
+
 
 def find_language_facts(languages, language_name, fact_index = 0)
   # Take languages (now with additional facts added to each language with the
@@ -50,4 +71,19 @@ def find_language_facts(languages, language_name, fact_index = 0)
   #                 initial_release: 'December 4, 1995',
   #                 is_beautiful?: false }
   # }
+  languages[language_name][:facts][fact_index]
+
 end
+
+hash = 
+{
+  ruby: { facts: ['was made for programmer happiness', 'its code is beautiful'],
+          initial_release: 'December 25, 1996',
+          is_beautiful?: true },
+
+  javascript: { facts: ['you have to use semicolons everywhere', "its real name isn't even javascript"],
+                initial_release: 'December 4, 1995',
+                is_beautiful?: false }
+}
+
+p find_language_facts(hash,:ruby)
